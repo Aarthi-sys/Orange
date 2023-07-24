b2766c540e703829f5cef5007f010eb84ef45594
Aarthi-sys <aarthidakshnamoorthi@gmail.com>
2023-07-24 21:20:43 +0530

    First code push

---
+++ src/com/git/A.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class A {
+
+	private void method1() {
+		System.out.println("clientId");
+	}
+	private void method2() {
+		System.out.println("clientName");
+	}
+	private void method3() {
+		System.out.println("clientEmailId");
+	}
+}
